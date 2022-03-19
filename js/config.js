a5c1548c972ccba9776cf7c7134bb979a31d24e4
Zhuguohua <zgh_email@163.com>
2022-03-19 16:57:38 +0800

    Site updated: 2022-03-19 16:57:36

--- js/config.js
+++ js/config.js
@@ -6,9 +6,14 @@ if (!window.NexT) window.NexT = {};
   const staticConfig = {};
   let variableConfig = {};
 
-  const parse = text => JSON.parse(text || '{}');
+  const parse = (text) => {
+    const jsonString = new DOMParser()
+      .parseFromString(text, 'text/html').documentElement
+      .textContent;
+    return JSON.parse(jsonString || '{}');
+  };
 
-  const update = name => {
+  const update = (name) => {
     const targetEle = document.querySelector(`.${className}[data-name="${name}"]`);
     if (!targetEle) return;
     const parsedConfig = parse(targetEle.text);
@@ -31,31 +36,20 @@ if (!window.NexT) window.NexT = {};
         existing = variableConfig[name];
       }
 
-      // For unset override and mixable existing
-      if (!(name in overrideConfig) && typeof existing === 'object') {
-        // Get ready to mix.
-        overrideConfig[name] = {};
+      let override = overrideConfig[name];
+      if (override === undefined && typeof existing === 'object') {
+        override = {};
+        overrideConfig[name] = override;
       }
 
-      if (name in overrideConfig) {
-        const override = overrideConfig[name];
-
-        // When mixable
-        if (typeof override === 'object' && typeof existing === 'object') {
-          // Mix, proxy changes to the override.
-          return new Proxy({ ...existing, ...override }, {
-            set(target, prop, value) {
-              target[prop] = value;
-              override[prop] = value;
-              return true;
-            }
-          });
-        }
-
-        return override;
+      if (typeof override === 'object') {
+        return new Proxy({...existing, ...override}, {
+          set(target, prop, value) {
+            override[prop] = value;
+            return true;
+          }
+        });
       }
-
-      // Only when not mixable and override hasn't been set.
       return existing;
     }
   });
--- js/third-party/chat/chatra.js
+++ js/third-party/chat/chatra.js
@@ -1,19 +1,10 @@
-/* global CONFIG, Chatra */
+/* global CONFIG */
 
-(function() {
-  if (CONFIG.chatra.embed) {
-    window.ChatraSetup = {
-      mode    : 'frame',
-      injectTo: CONFIG.chatra.embed
-    };
-  }
+if (CONFIG.chatra.embed) {
+  window.ChatraSetup = {
+    mode    : 'frame',
+    injectTo: CONFIG.chatra.embed
+  };
+}
 
-  window.ChatraID = CONFIG.chatra.id;
-
-  const chatButton = document.querySelector('.sidebar-button button');
-  if (chatButton) {
-    chatButton.addEventListener('click', () => {
-      Chatra('openChat', true);
-    });
-  }
-})();
+window.ChatraID = CONFIG.chatra.id;
